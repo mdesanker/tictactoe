13efa9da122ddf4a0f4feeb1ec9d6e38a7232bea
Michael Desanker <mdesanker@gmail.com>
2021-10-05 09:51:36 -0400

    feat(js): add gameboard module to control board modification

--- script.js
+++ script.js
@@ -12,3 +12,31 @@ const Player = sign => {
 
 const playerX = Player('X');
 console.log(playerX.getSign());
+
+// Gameboard module controls modification of gameboard
+const Gameboard = (() => {
+    let board = ['', 'X', '', '', '', 'O', '', '', ''];
+
+    const getSign = index => {
+        return board[index];
+    };
+
+    const addSign = (index, sign) => {
+        if (board[index] === '') {
+            board[index] = sign;
+        }
+    };
+
+    const reset = () => {
+        board.forEach((_, key) => {
+            board[key] = '';
+        });
+    };
+
+    return { getSign, addSign, reset, board };
+})();
+
+// Display controller module controls screen and gameplay
+const DisplayController = (() => {
+    // Code here
+})();
