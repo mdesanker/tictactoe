9355c2fb77ca2a69118ecbc199a443e61dd5ce2c
Michael Desanker <mdesanker@gmail.com>
2021-10-05 09:41:03 -0400

    feat(js): add factory function to generate player objects

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 // script.js //
-"use strict";
+'use strict';
+
+// Player factory function
+const Player = sign => {
+    this.sign = sign;
+    const getSign = () => {
+        return sign;
+    };
+    return { getSign };
+};
+
+const playerX = Player('X');
+console.log(playerX.getSign());
